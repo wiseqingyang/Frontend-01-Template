53d15540ee0f250bf9fafb47554db27ff9a36bfc
qingyang <wiseqingyang@163.com>
2020-05-20 02:32:53 +0800

    有限状态机

---
+++ week06/match.js
@@ -0,0 +1,65 @@
+// match abababx
+function match(string) {
+    let state = start;
+
+    for (let c of string) {
+        state = state(c);
+    }
+    return state === end;
+}
+
+function start(c) {
+    if (c === 'a') {
+        return foundA;
+    } else {
+        return start;
+    }
+}
+
+function foundA(c) {
+    if (c === 'b') 
+        return foundB;
+    else 
+        return start(c);
+}
+
+function foundB(c) {
+    if (c === 'a')
+        return foundA2;
+    else
+        return start(c);
+}
+
+function foundA2(c) {
+    if (c === 'b') 
+        return foundB2;
+    else 
+        return start(c);
+}
+
+function foundB2(c) {
+    if (c === 'a')
+        return foundA3;
+    else
+        return foundB(c);
+}
+
+function foundA3(c) {
+    if (c === 'b')
+        return foundB3;
+    else
+        return foundA2(c);
+}
+
+function foundB3(c) {
+    if (c === 'x') 
+        return end;
+    else
+        return foundB2(c);
+}
+
+function end(c) {
+    return end;
+}
+
+console.log(match('sdjaabaababbabababxaabbbaabaababaaabbx'))
